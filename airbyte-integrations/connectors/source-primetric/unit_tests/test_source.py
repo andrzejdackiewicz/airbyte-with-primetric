97dff93ef072cd590f5fcc2b7637940f32d77b82
Andrzej Dackiewicz <andrzej.dackiewicz@getindata.com>
2022-08-10 15:13:48 +0200

    IA-1 updated tests

--- airbyte-integrations/connectors/source-primetric/unit_tests/test_source.py
+++ airbyte-integrations/connectors/source-primetric/unit_tests/test_source.py
@@ -1,21 +1,28 @@
 #
 # Copyright (c) 2022 Airbyte, Inc., all rights reserved.
 #
-
+from typing import Mapping, Any
 from unittest.mock import MagicMock
-
 from source_primetric.source import SourcePrimetric
+import json
+
+
+def get_config() -> Mapping[str, Any]:
+    filename = "secrets/config.json"
+    with open(filename) as json_file:
+        return json.load(json_file)
 
 
 def test_check_connection(mocker):
     source = SourcePrimetric()
-    logger_mock, config_mock = MagicMock(), MagicMock()
+    logger_mock = MagicMock()
+    config_mock = get_config()
     assert source.check_connection(logger_mock, config_mock) == (True, None)
 
 
 def test_streams(mocker):
     source = SourcePrimetric()
-    config_mock = MagicMock()
+    config_mock = get_config()
     streams = source.streams(config_mock)
     expected_streams_number = 3
     assert len(streams) == expected_streams_number
